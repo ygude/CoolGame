d31503e9b7334b9163c903a7590222baa0c12c2a
Yesubabu Gude <yesugude@amd.com>
2022-07-15 23:21:59 +0530

    Initial commit

---
+++ game/src/test/test_GameObject.cpp
@@ -0,0 +1,16 @@
+#include "GameObject.h"
+
+#include <gtest/gtest.h>
+
+TEST(GameObjectTest, CreateObject)
+{
+	CoolGame::GameObject gameObject("Hello World");
+	ASSERT_EQ(gameObject.getName(), "Hello World");	
+}
+
+TEST(GameObjectTest, FailTest)
+{
+	std::cout << "Hello World" << std::endl;
+	ASSERT_EQ(false, true);
+}
+
