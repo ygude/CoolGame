d10c149c6804f414c79f1883fd01bf0c1d81254b
Yesubabu Gude <yesugude@amd.com>
2022-07-18 22:14:51 +0530

    verify tests script

--- verify_tests.py
+++ verify_tests.py
@@ -1,8 +1,11 @@
 import sys
+import os
 from subprocess import Popen, PIPE
 
 test_name = "CoolGame_tst"
 tests_dir = "build/game/src/test"
+script_path = os.path.abspath( __file__ )
+err_log = script_path + "/" +test_name + "_err.log"
 test_cmd = "./{}/{}".format(tests_dir, test_name)
 process1 = Popen(test_cmd, stdout=PIPE, stderr=PIPE, shell=True)
 stdout, stderr = process1.communicate()
@@ -11,6 +14,6 @@ if process1.returncode == 0:
     print("Tests are passed")
 else:
     print("Tests are failed......................!")
-    with open(test_name + "_err.log") as fd:
+    with open(err_log) as fd:
         fd.write(stderr.decode(encode='utf-8'))
 
